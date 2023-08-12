790fb40a7dc389a418a8b9d761886f1275dfd0c1
Per <vbratou@gmail.com>
2023-08-12 23:15:25 +0200

    Update periodic_table.sql

--- periodic_table.sql
+++ periodic_table.sql
@@ -1,1 +1,139 @@
+--
+-- PostgreSQL database dump
+--
 
+-- Dumped from database version 12.9 (Ubuntu 12.9-2.pgdg20.04+1)
+-- Dumped by pg_dump version 12.9 (Ubuntu 12.9-2.pgdg20.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP DATABASE periodic_table;
+--
+-- Name: periodic_table; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE periodic_table WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
+
+
+ALTER DATABASE periodic_table OWNER TO postgres;
+
+\connect periodic_table
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: elements; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.elements (
+    atomic_number integer NOT NULL,
+    symbol character varying(2) NOT NULL,
+    name character varying(40) NOT NULL
+);
+
+
+ALTER TABLE public.elements OWNER TO freecodecamp;
+
+--
+-- Name: properties; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.properties (
+    atomic_number integer NOT NULL,
+    atomic_mass real NOT NULL,
+    melting_point_celsius numeric NOT NULL,
+    boiling_point_celsius numeric NOT NULL,
+    type_id integer NOT NULL
+);
+
+
+ALTER TABLE public.properties OWNER TO freecodecamp;
+
+--
+-- Name: properties_type_id_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
+--
+
+CREATE SEQUENCE public.properties_type_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.properties_type_id_seq OWNER TO freecodecamp;
+
+--
+-- Name: properties_type_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
+--
+
+ALTER SEQUENCE public.properties_type_id_seq OWNED BY public.properties.type_id;
+
+
+--
+-- Name: types; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.types (
+    type_id integer NOT NULL,
+    type character varying(50) NOT NULL
+);
+
+
+ALTER TABLE public.types OWNER TO freecodecamp;
+
+--
+-- Name: types_type_id_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
+--
+
+CREATE SEQUENCE public.types_type_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.types_type_id_seq OWNER TO freecodecamp;
+
+--
+-- Name: types_type_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
+--
+
+ALTER SEQUENCE public.types_type_id_seq OWNED BY public.types.type_id;
+
+
+--
+-- Name: properties type_id; Type: DEFAULT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.properties ALTER COLUMN type_id SET DEFAULT nextval('public.properties_type_id_seq'::regclass);
+
+
+--
+-- Name: types type_id; Type: DEFAULT; Schema: public; Owner: freecodecamp
+--
